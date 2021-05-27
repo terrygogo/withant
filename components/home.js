59d792bd4dcd27e612be7bb9b5f500fd7408617e
terry <terry@jionlab.co.kr>
2021-05-27 13:19:48 +0900

    update for sync

--- components/home.js
+++ components/home.js
@@ -1,100 +1,77 @@
-  
-import _ from "lodash";
-
-//import   "./home.module.css"
-//import "../node_modules/react-grid-layout/css/styles.css"
-//import '../node_modules/react-resizable/css/styles.css'
-
 import React from "react";
-import { WidthProvider, Responsive } from "react-grid-layout";
-import   User  from '../pages/user.js'
-import   Setting  from '../pages/setting.js'
-import   Dashboard  from '../pages/dashboard.js'
+import RGL, { WidthProvider } from "react-grid-layout";
+const ReactGridLayout = WidthProvider(RGL);
+ 
+export default class ErrorCaseLayout extends React.PureComponent {
+  static defaultProps = {
+    className: "layout",
+    items: 3,
+    rowHeight: 100,
+    onLayoutChange: function() {},
+    cols: 2
+  };
 
-const ResponsiveReactGridLayout = WidthProvider(Responsive);
-const originalLayouts = getFromLS("layouts") || {};
-
-/**
- * This layout demonstrates how to sync multiple responsive layouts to localstorage.
- */
-export default class Home    extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.state = {
-      layouts: JSON.parse(JSON.stringify(originalLayouts))
-    };
+    const layout = this.generateLayout();
+    this.state = { layout };
   }
 
-  static get defaultProps() {
-    return {
-      className: "layout",
-      cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
-      rowHeight: 30
-    };
+  generateDOM() {
+    return [
+      <div key={"1"}>
+        <span className="react-grid-item">{"1"}</span>
+      </div>,
+      <div key={"2"}>
+        <span className="text">{"2"}</span>
+      </div>,
+      <div key={"3"}>
+        <span className="text">{"3"}</span>
+      </div>
+    ];
   }
 
-  resetLayout() {
-    this.setState({ layouts: {} });
+  generateLayout() {
+    return [
+      {
+        x: 0,
+        y: 0,
+        w: 1,
+        h: 1,
+        i: "1"
+      },
+      {
+        x: 1,
+        y: 0,
+        w: 1,
+        h: 1,
+        i: "2"
+      },
+      {
+        x: 0,
+        y: 1,
+        w: 2,
+        h: 2,
+        i: "3"
+      }
+    ];
   }
 
-  onLayoutChange(layout, layouts) {
-    saveToLS("layouts", layouts);
-    this.setState({ layouts });
+  onLayoutChange(layout) {
+    this.props.onLayoutChange(layout);
   }
 
   render() {
     return (
-      <div>
-          
-           
-        <button onClick={() => this.resetLayout()}>Reset Layout</button>
-        <ResponsiveReactGridLayout
-        className= "Layo1ut"
-     
-          cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-          rowHeight={30}
-          layouts={this.state.layouts}
-          onLayoutChange={(layout, layouts) =>
-            this.onLayoutChange(layout, layouts)
-          }
-        >
-          
-            <Setting  key="1" data-grid={{ w: 4, h: 2, x: 0, y: 0, minW: 2, minH: 4 }}> </Setting>
-          
-         
-            <User key="2" data-grid={{ w:2, h: 4, x: 0, y: 2, minW: 2, minH: 4 }}></User>
-          
-        
-            <Dashboard key="3" data-grid={{ w: 2, h: 4, x: 4, y: 2, minW: 2, minH: 4 }}></Dashboard>
-          
-           
-        </ResponsiveReactGridLayout>
-      </div>
+      <ReactGridLayout
+      className="layout" 
+        layout={this.state.layout}
+        onLayoutChange={this.onLayoutChange}
+        {...this.props}
+      >
+        {this.generateDOM()}
+      </ReactGridLayout>
     );
   }
 }
-
-function getFromLS(key) {
-  let ls = {};
-  if (global.localStorage) {
-    try {
-      ls = JSON.parse(global.localStorage.getItem("rgl-8")) || {};
-    } catch (e) {
-      /*Ignore*/
-    }
-  }
-  return ls[key];
-}
-
-function saveToLS(key, value) {
-  if (global.localStorage) {
-    global.localStorage.setItem(
-      "rgl-8",
-      JSON.stringify({
-        [key]: value
-      })
-    );
-  }
-}
- 
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
+import "react-grid-layout/css/styles.css"
+import 'react-resizable/css/styles.css'
+import '../styles/antdsample.css'
 import 'antd/dist/antd.css'
 import '../styles/vars.css'
 import '../styles/global.css'
-import '../styles/antdsample.css'
 
-import "react-grid-layout/css/styles.css"
-import 'react-resizable/css/styles.css'
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
